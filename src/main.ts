3263d1489f8b1deb760f767256d46cd1ef04784a
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-08-24 11:14:58 +0200

    chgt avant merge

--- src/main.ts
+++ src/main.ts
@@ -61,9 +61,9 @@ function initi(){
     console.log('init')
     butnStart.remove();
     body.appendChild(btnReplay);
-    
-    appli.innerHTML = `<p>Vous avez fait ${partieFait} partie(s)</p>`
     appli.appendChild(jeuDiv);
+    appli.innerHTML = `<p>Vous avez fait ${partieFait} partie(s)</p>`
+    
    
     compt++;
     //appli.innerHTML = `
