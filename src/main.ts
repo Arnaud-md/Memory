06a07a84e4e8bc475628824ffe6ace3e22ce20fd
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-08-25 15:14:49 +0200

    commencement du nettoyage du code avant Review

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 let compt = 0;
 let partieFait = 0;
-const colores = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 //const butnStart = document.querySelector("#init-button") as HTMLButtonElement;
 //const butnTile = document.querySelector(".tile") as HTMLButtonElement;
 // Attention aux "as" qui règlent pas mal de soucis
@@ -64,10 +64,7 @@ const head = document.querySelector("#col") as HTMLDivElement
 
 const butnStart = document.createElement('button') as HTMLButtonElement;
 butnStart.innerText = "Commencer la partie";
-//(typeof butnStart.textContent("Commencer la partie")==='string') && butnStart.textContent("Commencer la partie");
-//butnStart.addEventListener("click", () => {
-    //initi();
-//});
+
 appli.appendChild(butnStart);
 
 const butnRemise = document.createElement("button") as HTMLButtonElement;
@@ -103,31 +100,18 @@ fetch("https://dog.ceo/api/breeds/image/random")
     document.querySelector("#image-chat")?.setAttribute("src", data.message);
 })
 
-
-
-// Add an event listener
-//butnStart.addEventListener("click", () => {
-//    initi();
-//});
-
-
 const tiles = new Array(16).fill('').map( (_, i) => {
     const tile = document.createElement("div")
     tile.setAttribute("class", "tile")
     tile.style.width ="50px"
     tile.style.height = "50px"
     tile.style.margin = "25px 25px 25px 25px";
-    tile.style.backgroundColor = colores[Math.floor(i/2)]
+    tile.style.backgroundColor = colors[Math.floor(i/2)]
     return tile
 })
 
-    // Shuffle the tiles
-    tiles.sort( () => Math.random() - 0.5)
-
-    // Add the tiles to the app
-    //tiles.forEach( tile => appli.appendChild(tile))
-
-
+// Shuffle the tiles
+tiles.sort( () => Math.random() - 0.5)
 
 // Add an event listener
 butnStart.addEventListener("click", () => {
@@ -167,7 +151,7 @@ function initi(){
     compt++;
     let color1 = "1";
     let color2 = "2";
-    let couleur=colores;
+    let couleur=colors;
     appli.appendChild(jeuDiv);
     //appli.appendChild(butnRemise);
 
@@ -180,21 +164,20 @@ function initi(){
             
         }
     }
-
+// Add the tiles to the app
     tiles.forEach( tile => jeuDiv.appendChild(tile));
 
-    // Add the tiles to the app
     let nodeList = document.querySelectorAll(".tile");
     let elements = Array.from(nodeList);
     elements.forEach( (element) => {
-        //let i = 0;
+
         element.setAttribute("class", "not-revealed")
         
         element.addEventListener("click", () => {
             element.setAttribute("class", "revealed");
-            //color1 = getColor(tiles);
+
             let index = getIndex(tiles);
-            //appli.innerHTML = '<p>' + color1+'</p>'
+
             if (index!=-1) {
                 couleur[index]=tiles[index].style.backgroundColor;
                 tiles[index].style.backgroundColor = "white";
@@ -204,99 +187,6 @@ function initi(){
     })
     
 }
-    //appli.innerHTML = `
-    //    <p>${cpt}</p>
-    //    <button id="init-button">Nouveau Click</button>
-    //`;
-    //appli.innerHTML = '
-    //    <div id = "disp">
-    //';
-
-//    tiles.forEach( tile => jeuDiv.appendChild(tile));
-   
-    
-    // Add the tiles to the app
-//    let nodeList = document.querySelectorAll(".tile");
-//    let elements = Array.from(nodeList);
-//    elements.forEach( (element) => {
-        //let i = 0;
-//        element.setAttribute("class", "not-revealed")
-//        element.addEventListener("click", () => {
-//            element.setAttribute("class", "revealed");
-//            color1 = getColor(tiles);
-//            let index = getIndex(tiles);
-            //appli.innerHTML = '<p>' + color1+'</p>'
-//            if (index!=-1) {
-//                tiles[index].style.backgroundColor = "white";
-//            }
-//        })
-            ///showColor(tiles);
-            
-
-
-            ///tiles.forEach( tile => appli.appendChild(tile));
-
-            ///let nodeList2 = document.querySelectorAll(".tile");
-            ///let elements2 = Array.from(nodeList2);
-            ///elements2.forEach( (element2) => {
-                //let i = 0;
-                ///element2.setAttribute("class", "not-revealed2")
-                ///element2.addEventListener("click", () => {
-                    ///element2.setAttribute("class", "revealed2");
-                    ////showColor(tiles);
-                    ///color2 = getColor2(tiles);
-                    ///let index2 = getIndex2(tiles);
-                    ///if (color1==color2) {
-                        ///appli.innerHTML = '<p>les deux couleurs sont identiques</p>'
-                    ///}
-                    ///else {
-                        ///appli.innerHTML = '<p>les deux couleurs ne sont pas identiques</p>'
-                    ///}
-                ///})
-            ///})
-
-        //)
-        
-        //i++;
-        //element.addEventListener("click", () => {
-            //element.setAttribute("class", "revealed")
-        //})
-    ///})
-
-
-    
-    
-    
-    //tiles.forEach( tile => tile.addEventListener("click", () => {
-//        tiles.forEach( element => element.addEventListener("click", () => {
-        //const selectTile = document.querySelectorAll(".tile") as HTMLElement;
-   //     const selectTile = document.querySelectorAll(".tile") as HTMLDivElement;
-        //const colorTile = document.querySelector(".tile") as HTMLElement;
-        //const colorTile = tiles[i] as HTMLElement;
-//        card(element);
-//    }));
-    //let i : number;
-    //const butnTile=[];
-//    for (i=0;i<tiles.length;i++) {
-        //const butnTile = document.querySelector(".tile") as HTMLButtonElement;
-//        butnTile[i] = document.querySelector(".tile") as HTMLButtonElement;
-//        console.log("gagne");
-        
-//        butnTile[i].addEventListener("click", () => {
-//            console.log("gagne!");
-//            card(i);
-//        });
-//    };
-//}
-
-//function card() {
-//    appli.innerHTML = '<p>tata</p>';
-//}
-
-///function showColor(til:HTMLDivElement) {
-///    console.log('toto')
-///    appli.innerHTML = '<p>' + til.style.backgroundColor+'</p>';
-///}
 
 function showColor(tiles:Array<HTMLDivElement>) {
     for (let i=0;i<tiles.length;i++) {
@@ -306,13 +196,9 @@ function showColor(tiles:Array<HTMLDivElement>) {
     }
 }
 
-//function getColor(til:HTMLDivElement) {
-    ///    console.log('toto')
-    //return til.style.backgroundColor;
-//}
 
 function getColor(tiles:Array<HTMLDivElement>) {
-    ///    console.log('toto')
+
     let clr = "";
     for (let i=0;i<tiles.length;i++) {
         if (tiles[i].className=="revealed") {
@@ -323,7 +209,7 @@ function getColor(tiles:Array<HTMLDivElement>) {
 }
 
 function getColor2(tiles:Array<HTMLDivElement>) {
-    ///    console.log('toto')
+
     let clr = "";
     for (let i=0;i<tiles.length;i++) {
         if (tiles[i].className=="revealed2") {
